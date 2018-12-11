baff67c1d7e1eeda626bfd424a330970aab47a73
Martin Jedwabny <martiniedva@gmail.com>
2018-12-11 23:10:12 +0100

    Error checking on filter parsing

--- src/main/java/io/reader/BallotReader.java
+++ src/main/java/io/reader/BallotReader.java
@@ -1,5 +1,6 @@
 package main.java.io.reader;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -18,6 +19,7 @@ public class BallotReader {
 	 */
 	public static Ballot fromString(String ballotString, Map<String, Alternative> alternatives) {
 		Map<Alternative, Integer> rankForElement = new HashMap<Alternative, Integer>();
+		Collection<Alternative> alternativesLeft = alternatives.values();
 		try {
 			Matcher m = Pattern.compile("[0-9]+=[^0-9\\}]*").matcher(ballotString.replaceAll("\\s+",""));
 			while (m.find()) {
@@ -26,14 +28,21 @@ public class BallotReader {
 				Integer rank = Integer.parseInt(rankAndArray[0]);
 				String elementArrayString = rankAndArray[1];
 				String[] elementArray = elementArrayString.substring(
-						elementArrayString.indexOf("["), 
+						elementArrayString.indexOf("[")+1, 
 						elementArrayString.indexOf("]")).split(",");
-				for (String elementString : elementArray)
-					rankForElement.put(alternatives.get(elementString), rank);
+				for (String elementString : elementArray) {
+					Alternative chosen = alternatives.get(elementString);
+					if (chosen == null)
+						return null;
+					rankForElement.put(chosen, rank);
+					alternativesLeft.remove(chosen);
+				}
 			}
 		} catch (Exception e) {
 			return null;
 		}
+		if (!alternativesLeft.isEmpty())
+			return null;
 		return new Ballot(rankForElement);
 	}
 
