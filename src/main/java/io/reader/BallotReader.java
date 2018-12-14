e16e17610591ee094d116199d7637c4d5302f842
Martin Jedwabny <martiniedva@gmail.com>
2018-12-14 16:37:13 +0100

    Fix more Ballot Reader bugs

--- src/main/java/io/reader/BallotReader.java
+++ src/main/java/io/reader/BallotReader.java
@@ -23,17 +23,21 @@ public class BallotReader {
 		LinkedHashSet<Alternative> alternativesLeft = new LinkedHashSet<Alternative>();
 		alternativesLeft.addAll(alternatives);
 		try {
-			Matcher m = Pattern.compile("[0-9]+=[^0-9\\}]*").matcher(ballotString.replaceAll("\\s+",""));
+			Matcher m = Pattern.compile("[0-9]+=[^0-9\\}]*").matcher(ballotString.trim());
 			Integer rank = 1;
 			while (m.find()) {
 				String occur = m.group();
 				String[] rankAndArray = occur.split("=");
 				String elementArrayString = rankAndArray[1];
-				String[] elementArray = elementArrayString.substring(
+				elementArrayString = elementArrayString.substring(
 						elementArrayString.indexOf("[")+1, 
-						elementArrayString.indexOf("]")).split(",");
+						elementArrayString.indexOf("]")).trim();
+				System.out.println(elementArrayString);
+				if (elementArrayString.isEmpty())
+					continue;
+				String[] elementArray = elementArrayString.split(",");
 				for (String elementString : elementArray) {
-					Alternative chosen = nameToAlternative.get(elementString);
+					Alternative chosen = nameToAlternative.get(elementString.trim());
 					if (chosen == null)
 						return null;
 					rankForElement.put(chosen, rank);
